5795765dd5606fadb3d5023f42182f933c9735cf
Ksendel <oksana.karachun14@gmail.com>
2020-09-04 00:25:26 +0300

    fixed input validation

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../shared/interfaces';
 
 @Component({
   selector: 'app-login-page',
@@ -20,7 +21,8 @@ export class LoginPageComponent implements OnInit {
       ]),
       password: new FormControl(null, [
         Validators.required,
-        Validators.minLength(6)
+        Validators.minLength(6),
+        Validators.pattern("^[a-zA-Z]+$")
       ])
     })
   }
@@ -28,6 +30,11 @@ export class LoginPageComponent implements OnInit {
   submit() {
     if (this.form.invalid) {
       return
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
     }
   }
 }
